45ceb72231a9ef54caeb00b1d6754b9fc3ae45cf
Murray Legnain <78812393+mlegn098@users.noreply.github.com>
2023-03-03 16:11:17 -0500

    -updated to show survey created from lab1

--- docs/surveyController.js
+++ docs/surveyController.js
@@ -52,7 +52,7 @@ module.exports = function(app){
     // when a user goes to localhost:3000/niceSurvey
     // serve a static html (the survey itself to fill in)
     app.get('/niceSurvey', function(req, res){
-        res.sendFile(__dirname+'/views/niceSurvey.html');
+        res.sendFile(__dirname+'/views/index.html');
     });
 
     // when a user types SUBMIT in localhost:3000/niceSurvey 
