f5ab09dfa0221765f3bc311babeb34dd3308358d
Sheena Verana <67240854+sheenamrv@users.noreply.github.com>
2023-03-18 11:31:42 -0400

    Setup for lab 8 and add navbar

---
+++ docs/my-app/src/App.js
@@ -0,0 +1,23 @@
+// import logo from './logo.svg';
+
+import Navbar from "./components/Navbar";
+import "./App.css";
+
+export default function App()  {
+    return (
+      <div className="App">
+        <Navbar/>
+        <body/>
+      </div>
+    );
+  }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>CESAUO</h1>
+//     </div>
+//   );
+// }
+
+
---
+++ docs/my-app/src/components/Navbar.js
@@ -0,0 +1,28 @@
+import "./NavbarStyles.css";
+function Navbar() {
+  return (
+    <nav>
+      <a href="index.html">
+        CESAUO
+      </a>
+
+      <div>
+        <ul id="navbar">
+              <li><a href="index.html">About</a></li>
+              <li><a href="index.html">Team</a></li>
+              <li><a href="index.html">Events</a></li>
+              <li><a href="index.html">Courses</a></li>
+              <li><a href="index.html">FAQ</a></li>
+        </ul>
+      </div>
+
+
+    </nav>
+
+    
+    
+  )
+}
+
+
+export default Navbar;
