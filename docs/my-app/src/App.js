af1094ce9e7ccb1708be3c348ec5edba6aad3eb7
Saad Ahmed <email.saadahmed@gmail.com>
2023-03-19 20:28:11 -0400

    firstLandingPage

--- docs/my-app/src/App.js
+++ docs/my-app/src/App.js
@@ -2,11 +2,13 @@
 
 import Navbar from "./components/Navbar";
 import "./App.css";
+import Join from "./components/Join";
 
 export default function App()  {
     return (
       <div className="App">
         <Navbar/>
+        <Join/>
         <body/>
       </div>
     );
---
+++ docs/my-app/src/components/Join.js
@@ -0,0 +1,16 @@
+import "./JoinStyles.css";
+function Join() {
+  return (
+      <div class = "main">
+        
+        <img src="images/uottawa.png" class = "logo" ></img>
+        <div class = "button">
+		    <a href ="#" class = "btn" >General Member Registeration</a>
+        </div>
+
+      </div>
+
+  )
+}
+
+export default Join;
