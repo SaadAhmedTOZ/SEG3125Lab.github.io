c3984f375f593697dec6cfa485b98273516deea2
Saad Ahmed <email.saadahmed@gmail.com>
2023-03-25 16:09:43 -0400

    registerationDone

--- docs/my-app/src/components/Register.js
+++ docs/my-app/src/components/Register.js
@@ -3,7 +3,17 @@ function Register() {
   return (
       <div class = "box">
         General Member Registeration
+        <br />
+        <br />
+        <p class="head"> Please enter your name:</p>
 
+        <div class = "input" > 
+          <input type="text" required="required"/>
+        </div>
+
+        <a href ="/events" ><img src="images/arrowbutton.png"  class = "arrow"></img></a>
+
+        
       </div>
 
   )
