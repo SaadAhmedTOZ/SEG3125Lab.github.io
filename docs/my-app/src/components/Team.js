cfa07f8938d982cee0e497daf5d7a281b8fe51de
Sheena Verana <67240854+sheenamrv@users.noreply.github.com>
2023-03-26 18:57:11 -0400

    Update team page

--- docs/my-app/src/components/Team.js
+++ docs/my-app/src/components/Team.js
@@ -5,35 +5,39 @@ function Team() {
       <h1>Meet the Team 2023-2024</h1>
       <br />
       <br />
+      <table>
+        <tr>
+        <th>
+          <img src="images/person1.png" class="person"></img>
+          <h2>President</h2>
+          Martin Ross
+        </th>
+        <th>
+          <img src="images/person2.png" class="person"></img>
+          <h2>VP Internal</h2>
+          Anna Stone
+        </th>
+        <th>
+          <img src="images/person3.png" class="person"></img>
+          <h2>VP External </h2>
+          Hyrie Pineda
+        </th>
+        </tr>
+        <tr>
+        <th>
+          <img src="images/person4.png" class="person"></img>
+          <h2>VP Social </h2>
+          Leslie Luna
+        </th>
+        <th>
+          <img src="images/person5.png" class="person"></img>
+          <h2>VP Finance</h2>
+          John Park
+        </th>
+        </tr>
+      </table>
 
-      <img src="images/person1.png" class="person"></img>
-      <h2>President</h2>
-      Martin Ross
       
-      <br />
-      <br />
-
-      <img src="images/person2.png" class="person"></img>
-      <h2>VP Internal</h2>
-      Anna Stone
-      <br />
-      <br />
-
-      <img src="images/person3.png" class="person"></img>
-      <h2>VP External </h2>
-      Hyrie Pineda
-      <br />
-      <br />
-
-      <img src="images/person4.png" class="person"></img>
-      <h2>VP Social </h2>
-      Leslie Luna
-      <br />
-      <br />
-
-      <img src="images/person5.png" class="person"></img>
-      <h2>VP Finance</h2>
-      John Park
     
       
     </div>
