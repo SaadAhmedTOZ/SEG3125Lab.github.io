7f5d3f8999a8e1114860b386296202e83f7608fd
Sheena Verana <67240854+sheenamrv@users.noreply.github.com>
2023-03-26 20:47:32 -0400

    Update courses and events pages

--- docs/my-app/src/components/Courses.js
+++ docs/my-app/src/components/Courses.js
@@ -5,45 +5,45 @@ import "./CoursesStyles.css";
 function Courses() {
   return (
     <div>
-          <h2 class ="title-courses">3rd Year Courses</h2>
+          <h2 class ="title-courses">Courses</h2>
 <table class="center">
   <tr>
     <td>
-      <div class="course-1">
-        CEG 3136 &#10; Computer Architecture II
+      <div class="course-3">
+      CEG 4136 | Computer Architecture III
       </div>
     </td>
-    <td><div class="course-1">
-        Hack the Hill Hackathon
+    <td><div class="course-3">
+    CEG 4912 | Computer Engineering Design: Project I
       </div></td>
     <td><div class="course-3">
-        Hack the Hill Hackathon
+    CEG 4166 | Real-Time Systems Design
+      </div></td>
+  </tr>
+  <tr>
+  <td>
+  <div class="course-3">
+  CEG 4913 | Computer Engineering Design: Project II
+      </div>
+    </td>
+    <td><div class="course-3">
+    CEG 3136 | Computer Architecture II
+      </div></td>
+    <td><div class="course-3">
+    CEG 3155 | Digital Systems II
       </div></td>
   </tr>
   <tr>
   <td>
   <div class="course-2">
-        Hack the Hill Hackathon
+  ELG 3125 | Signal and System Analysis
       </div>
     </td>
-    <td><div class="course-3">
-        Hack the Hill Hackathon
-      </div></td>
     <td><div class="course-1">
-        Hack the Hill Hackathon
+    CSI 3131 | Operating Systems
       </div></td>
-  </tr>
-  <tr>
-  <td>
-  <div class="course-2">
-        Hack the Hill Hackathon
-      </div>
-    </td>
     <td><div class="course-2">
-        Hack the Hill Hackathon
-      </div></td>
-    <td><div class="course-3">
-        Hack the Hill Hackathon
+    ELG 3155 | Introduction to Control Systems
       </div></td>
   </tr>
 </table>
--- docs/my-app/src/components/Events.js
+++ docs/my-app/src/components/Events.js
@@ -5,14 +5,14 @@ import "./EventsStyles.css";
 function Events() {
   return (
     <div>
-          <h2 class ="title-events">Past Events</h2>
+          <h2 class ="title-events">Events</h2>
 <table class="center">
   <tr>
     <td>
       <div class="eventblur">
         <img src="images/hackthehill.png"></img>
         <h2>Hack the Hill Hackathon</h2>
-        <h3>March 37th 2035</h3>
+        <h3>March 27th 2023</h3>
         <p>Join us for our first ever Hackathon! We will be hosting a 24 hour hackathon where you can work with your friends to create a project that you are passionate about. We will be providing food, drinks, and prizes for the top 3 teams. </p>
       </div>
     </td>
@@ -20,16 +20,16 @@ function Events() {
         <img src="images/3155ReviewSession.png"></img>
         <h2>CEG3155 Review Session</h2>
         <h3>Feb 15th 2023</h3>
-        <p>If you need help prepping for the CEG3155 final...!dsafla</p>
+        <p>If you need help prepping for the CEG3155 final, join other students and review key concepts.</p>
       </div></td>
     <td><div class="eventblur">
         <img src="images/meettheexecs.png"></img>
         <h2>Meet the Execs Event</h2>
         <h3>Feb 15th 2023</h3>
-        <p>If you need help prepping for the CEG3155 final...!dsafla</p>
+        <p>Join our first event of the semester and get to know the people running CESAUO. </p>
       </div></td>
   </tr>
-  <tr>
+  {/* <tr>
   <td>
       <div class="eventblur">
         <img src="images/hackthehill.png"></img>
@@ -72,7 +72,7 @@ function Events() {
         <h3>Feb 15th 2023</h3>
         <p>If you need help prepping for the CEG3155 final...!dsafla</p>
       </div></td>
-  </tr>
+  </tr> */}
 </table>
     </div>
   );
